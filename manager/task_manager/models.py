196f83af0cc7c74a6d91d6de2dabd21d0bd5c74b
P.Seferyniak <p.seferyniak@gmail.com>
2024-07-04 23:41:47 +0200

    Create model Task

--- manager/task_manager/models.py
+++ manager/task_manager/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=30)
+    describe = models.TextField()
+    created = models.DateTimeField(default=timezone.now())
+    date_start = models.DateField()
+    date_end = models.DateField()
+    completed = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title

2f833d176f252504c71bcfd1eac6024c8b2cf978
P.Seferyniak <p.seferyniak@gmail.com>
2024-07-06 13:15:38 +0200

    Create admin panel, connected to Postgress SQL

--- manager/manager/settings.py
+++ manager/manager/settings.py
@@ -1,3 +1,5 @@
+
+
 """
 Django settings for manager project.
 
@@ -11,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from .pass_file import USER, NAME, PASSWORD
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,8 +82,10 @@ WSGI_APPLICATION = 'manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : 'django.db.backends.postgresql',
+        'NAME' : NAME,
+        'USER' : USER,
+        'PASSWORD' : PASSWORD,
     }
 }
 
